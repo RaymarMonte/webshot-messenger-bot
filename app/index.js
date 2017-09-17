cd02d1ef2048ac5813859b924fee1442e7e79681
Raymar Monte <raymar.v.monte@accemture.com>
2017-09-17 17:55:18 +0800

    Removed binaries. Added code for messenger bot.

--- app/index.js
+++ app/index.js
@@ -10,7 +10,11 @@ const app = express();
 
 // get environment dependent phantomjs path
 const WEBSHOT_OPTIONS = {
-  phantomPath: require ('phantomjs2').path
+  phantomPath: require ('phantomjs2').path,
+  shotSize: {
+    width: 'window',
+    height: 'all'
+  }
 }
 const APP_DIR = path.dirname(require.main.filename);
 
@@ -36,10 +40,83 @@ app.get('/', function (req, res) {
     });
   }
   else {
-    res.send("Give me a url through my 'page' parameter and I'll give you its"
+    res.send("Give me a url through my 'page' parameter and I'll give you a"
       + " screenshot ;)");
   }
 });
+
+// facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge']);
+	} else {
+	   res.send('Error, wrong token');
+  }
+})
+
+// message parser
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i];
+	    let sender = event.sender.id;
+	    if (event.message && event.message.text) {
+		    let text = event.message.text;
+		    getScreenshot(text, function(screenshot) {
+          if (screenshot) {
+            var screenshotPath = path.join(APP_DIR, screenshot);
+            sendImageMessage(sender, screenshotPath);
+          }
+          else {
+            sendTextMessage(sender, 'Hi! Please give me a valid URL so I can'
+              + ' give you its screenshot. Thank you :)');
+          }
+        })
+	    }
+    }
+    res.sendStatus(200)
+});
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
+function sendImageMessage(sender, imagePath) {
+    let messageData = { attachment: {
+      type: 'image',
+      payload: {}
+    }};
+    let fileData = imagePath + ';type=image/png';
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			  message: messageData,
+        filedata: fileData
+		}
+	}, function(error, response, body) {
+		if (!error) {
+		    fs.unlink(imagePath);
+		}
+    })
+}
 
 function getScreenshot(url, callback) {
   var filename = sanitize(url);
